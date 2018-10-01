e35dc48daf31037c33220869a96faed1492caded
nisargthakar <36208557+nisargthakar@users.noreply.github.com>
2018-10-01 08:59:57 -0500

    Add files via upload

---
+++ xbeeReceiver.ino
@@ -0,0 +1,66 @@
+//Variables
+bool started= false;//True: Message is strated
+bool ended 	= false;//True: Message is finished 
+char incomingByte ; //Variable to store the incoming byte
+char msg[3];		//Message - array from 0 to 2 (3 values - PWM - e.g. 240)
+byte index;			//Index of array
+
+void setup() {
+
+	//Start the serial communication
+	Serial.begin(9600); //Baud rate must be the same as is on xBee module
+
+}
+
+void loop() {
+	
+  while (Serial.available()>0){
+  	//Read the incoming byte
+    incomingByte = Serial.read();
+    //Serial.println(incomingByte);
+    //Start the message when the '<' symbol is received
+    if(incomingByte == '<')
+    {
+     started = true;
+     index = 0;
+     msg[index] = '\0'; // Throw away any incomplete packet
+   }
+   //End the message when the '>' symbol is received
+   else if(incomingByte == '>')
+   {
+     ended = true;
+     break; // Done reading - exit from while loop!
+   }
+
+   
+   //Read the message!
+   else
+   {
+     if(index < 4) // Make sure there is room
+     {
+       msg[index] = incomingByte; // Add char to array
+       index++;
+       msg[index] = '\0'; // Add NULL to end
+     }
+   }
+ }
+ 
+ if(started && ended)
+ {
+   int value = atoi(msg);
+   //Serial.println(value);
+   //analogWrite(ledPin, value);
+   int Celsius = (5.0*value*50.0)/1024;
+   int Farenheit = (9.0/5.0)*Celsius +32;
+   Serial.print("C= ");
+   Serial.println(Celsius);
+   Serial.print("F= ");
+   Serial.println(Farenheit);
+   index = 0;
+   msg[index] = '\0';
+   started = false;
+   ended = false;
+ }
+
+}
+  
---
+++ xbeeTransmiter.ino
@@ -0,0 +1,28 @@
+//Constants: 
+const int tempPin = A0; //Pot at Arduino A0 pin 
+//Variables:
+int value ; //Value from pot
+
+void setup() {
+
+	//Start the serial communication
+	Serial.begin(9600); //Baud rate must be the same as is on xBee module
+
+}
+
+void loop() {
+  	
+  	//Read the analog value from pot and store it to "value" variable
+  	value = analogRead(A0);
+  	//Map the analog value to pwm value
+  	value = map (value, 0, 1023, 0, 255);
+	//Send the message:
+	Serial.print('<'); 	//Starting symbol
+ 	Serial.print(value);//Value from 0 to 255
+	Serial.print('>');//Ending symbol
+ /* Serial.print('!');
+  Serial.print(value-8);
+  Serial.println('?'); */
+  delay(500);
+}
+  
